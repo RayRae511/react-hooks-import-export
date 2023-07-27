f14371e8d5ddea9c50f1444714b7ef0ccc37c20d
Raymond Oeta <oetaray@gmail.com>
2023-07-27 12:42:05 +0300

    completed work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
